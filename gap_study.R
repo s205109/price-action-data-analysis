b1e66ceaab517231d6b8de4597311bbb5e4e36ff
Dom Sciarrino <dominic.sciarrino@gmail.com>
2021-11-19 17:43:15 -0500

    Added beta studies for stop loss percentage

--- gap_study.R
+++ gap_study.R
@@ -43,7 +43,7 @@ gap_strategy_summary <- data.frame(Ticker=character(),
                                    StandardDev=double())
 
 
-STOP_LOSS_PERCENT <- 0.10
+STOP_LOSS_PERCENT <- 1
 
 for (k in seq(from=1, to=length(all_files))){
   # Get CSV pricing data
@@ -147,7 +147,7 @@ for (k in seq(from=1, to=length(all_files))){
 
     # Track when we're in a trade
     trade_execution_status <- c(trade_execution_status, trade_currently_executed)
-    
+
   }
 
   ##########################################
@@ -182,7 +182,36 @@ mean(as.double(gap_strategy_summary$Drawdown))
 
 
 
+##########################################
+#        Varying beta (stop loss)        #
+##########################################
 
+beta <- c(0.05 , 0.10 , 0.15 , 0.20 , 0.25 , 0.30 , 0.35 , 0.40 , 0.45 , 0.50 , 0.60 , 0.70 , 0.80 , 0.90 , 1.00)
+rets <- c(1.158 , 1.330, 1.359 , 1.429 , 1.501 , 1.530 , 1.605, 1.666 , 1.732 , 1.822 , 1.779 , 1.905 , 1.911 , 1.911 , 1.911)
+stddevs <- c(0.00414 , 0.00628 , 0.00766 , 0.00843 , 0.00918 , 0.00969 , 0.01012 , 0.01042 , 0.01088, 0.01132 , 0.0118 , 0.01172 , 0.01175, 0.01175, 0.01175)
+maxdds <- c(-29.114 , -39.593 , -39.699 , -40.197 , -39.672 , -38.708 , -37.922 , -37.396 , -37.012 , -35.875 , -38.327 , -35.665 , -35.733 , -35.733 , -35.733)
+
+beta_rets_df <- data.frame(beta, rets)
+beta_stddevs_df <- data.frame(beta, stddevs)
+beta_maxdd_df <- data.frame(beta, maxdds)
+
+ggplot(beta_rets_df, aes(x=beta, y=rets)) +
+  geom_line() +
+  ggtitle(expression(paste(beta, " vs Returns for Gap Down Strategy"))) +
+  ylab("Returns") +
+  xlab(expression(paste("Stop Loss Percent ",beta)))
+
+ggplot(beta_stddevs_df, aes(x=beta, y=stddevs)) + 
+  geom_line() +
+  ggtitle(expression(paste(beta, " vs Standard Deviations for Gap Down Strategy"))) +
+  ylab("Standard Deviation") +
+  xlab(expression(paste("Stop Loss Percent ", beta)))
+
+ggplot(beta_maxdd_df, aes(x=beta, y=maxdds)) + 
+  geom_line() +
+  ggtitle(expression(paste(beta, " vs Maximum Drawdown for Gap Down Strategy"))) +
+  ylab("Maximum Drawdown") +
+  xlab(expression(paste("Stop Loss Percent ", beta)))
 
 
 
